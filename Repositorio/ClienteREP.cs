3b7f730d2d990f1433d62f055824b0acb95f41d3
MARINA ANDRÉ CHRISPIM <38954777880@jpmorgan.sp>
2017-11-17 20:35:42 -0200

    Criação de classes dentro dos  repositórios

---
+++ Repositorio/ClienteREP.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Dominio;
+
+namespace Repositorio
+{
+    public class ClienteREP : Cliente , ICadastro
+    {
+        
+    }
+}
