e17353d1d4020cf197a95821f83cf31a10e1ade4
Nina Chrispim <marina.chrispim23@gmail.com>
2017-11-17 22:01:45 -0200

    Método cadastrar cliente

--- Repositorio/ClienteREP.cs
+++ Repositorio/ClienteREP.cs
@@ -6,12 +6,34 @@ using Dominio;
 
 namespace Repositorio
 {
-    public class ClienteREP : Pessoa, ICadastro
+    public class ClienteREP : Pessoa, ICadastro<Cliente>
     {
-        public string Cadastrar(object objeto)
+        public bool Cadastrar(Cliente objeto)
         {
-            throw new NotImplementedException();
-        }
+            bool efetuado = false;
+            StreamWriter arquivo = null;
+
+            try{
+
+                //O "true" faz com o arquivo crie anexos, para que fiquem mais informações no mesmo arquivo;
+                arquivo = new StreamWriter("cliente.csv",true);
+                arquivo.WriteLine(Nome+";"+Telefone+";"+ Documento+";"+ Email);
+                efetuado = true;
+                      
+            }
+
+            catch(Exception erro){
+                throw new Exception("Erro ao tentar gravar o arquivo"+erro.Message);
+            }
+
+
+            finally{
+                arquivo.Close();
+            }
+
+            return efetuado; }
+        
+        
 
         public string Pesquisar(object objeto)
         {
