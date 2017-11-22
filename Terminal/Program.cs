7965dbdbca0e9f86a7a06b28a5db7627723e4f64
Alander Machado <alandermm@hotmail.com>
2017-11-21 22:01:14 -0200

    Menu cadastro Ok

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -8,7 +8,7 @@ namespace Terminal
     {
         static void Main(string[] args)
         {
-
+            menuPrincipal();
         }
 
         static void menuPrincipal(){
@@ -16,75 +16,127 @@ namespace Terminal
             do{
                 Console.WriteLine(
                     "Escolha uma das opções abaixo:" +
-                    "1 - Cadastrar" +
-                    "2 - Vender" +
-                    "3 - Comprar" +
-                    "4 - Histórico" +
-                    "9 - Sair"
+                    "\n1 - Cadastrar" +
+                    "\n2 - Vender" +
+                    "\n3 - Comprar" +
+                    "\n4 - Histórico" +
+                    "\n9 - Sair"
                 );
                 opt = Int16.Parse(Console.ReadLine());
-            }while (opt != 9);
-            
-            switch (opt)
-            {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 9: 
-                    Environment.Exit(0);
-                    break;
-            }
 
-            Cliente pessoa = new Cliente();
-
-            
-            new ClienteREP().Cadastrar(pessoa);
+                switch (opt)
+                {
+                    case 1:
+                        menuCadastro();
+                        break;
+                    case 2:
+                        formaPagamento();
+                        break;
+                    case 3:
+                        formaPagamento();
+                        break;
+                    case 4:
+                        menuHistorico();
+                        break;
+                    case 9: 
+                        Environment.Exit(0);
+                        break;
+                }
+            }while (opt != 9);   
         }
         
         static void menuCadastro(){
             int opt = 0;
             do{
                 Console.WriteLine(
-                    "Escolha uma das opções abaixo:" +
-                    "1 - Cadastrar Produto" +
-                    "2 - Cadastrar Cliente" +
-                    "3 - Cadastrar Fornecedor" +
-                    "9 - Voltar ao menu anterior"
+                    "\nEscolha uma das opções abaixo:" +
+                    "\n1 - Cadastrar Produto" +
+                    "\n2 - Cadastrar Cliente" +
+                    "\n3 - Cadastrar Fornecedor" +
+                    "\n9 - Voltar ao menu anterior"
                 );
                 opt = Int16.Parse(Console.ReadLine());
+
+                switch(opt){
+                    case 1:{
+                        bool cadastrado;
+                        do{
+                            Console.Write("Digite o código de barras: ");
+                            string codigoBarras = Console.ReadLine();
+                            Console.Write("Digite o Nome do produto: ");
+                            string nomeProduto = Console.ReadLine();
+                            Console.Write("Digite a descrição do produto: ");
+                            string descricao = Console.ReadLine();
+                            Console.Write("Digite o preço: ");
+                            double preco = double.Parse(Console.ReadLine());
+                            Produto produto = new Produto(codigoBarras, nomeProduto, descricao, preco);
+                            cadastrado = new ProdutoREP().Cadastrar(produto);
+                        } while(!cadastrado);
+                        break;
+                    }
+                    case 2: {
+                        bool cadastrado;
+                        do{
+                            Console.Write("Digite o nome do cliente: ");
+                            string nome = Console.ReadLine();
+                            Console.Write("Digite o documento do cliente: ");
+                            string documento = Console.ReadLine();
+                            Console.Write("Digite o telefone: ");
+                            string telefone = Console.ReadLine();
+                            Console.Write("Digite o e-mail: ");
+                            string email = Console.ReadLine();
+                            Cliente cliente = new Cliente(nome, telefone, documento, email);
+                            cadastrado = new ClienteREP().Cadastrar(cliente);
+                        } while(!cadastrado);
+                        break;
+                    } 
+                    case 3:
+                        bool cadastrado;
+                        do{
+                            Console.Write("Digite o nome do fornecedor: ");
+                            string nome = Console.ReadLine();
+                            Console.Write("Digite o documento do fornecedor: ");
+                            string documento = Console.ReadLine();
+                            Console.Write("Digite o telefone: ");
+                            string telefone = Console.ReadLine();
+                            Console.Write("Digite o e-mail: ");
+                            string email = Console.ReadLine();
+                            Fornecedor fornecedor = new Fornecedor(nome, telefone, documento, email);
+                            cadastrado = new FornecedorREP().Cadastrar(fornecedor);
+                        } while(!cadastrado);
+                        break;
+                    case 9:
+
+                        break;
+                }
             }while (opt != 9);
         }
 
         static void formaPagamento(){
-            int opt = 0;
+            int opt3 = 0;
             do{
                 Console.WriteLine(
                     "Escolha a forma de pagamento:" +
-                    "1 - À Vista" +
-                    "2 - Debito" +
-                    "3 - Credito" +
-                    "9 - Voltar ao menu anterior"
+                    "\n1 - À Vista" +
+                    "\n2 - Debito" +
+                    "\n3 - Credito" +
+                    "\n9 - Voltar ao menu anterior"
                 );
-                opt = Int16.Parse(Console.ReadLine());
-            }while (opt != 9);
+                opt3 = Int16.Parse(Console.ReadLine());
+            }while (opt3 != 9);
         }
 
         static void menuHistorico(){
-            int opt = 0;
+            int opt4 = 0;
             do{
                 Console.WriteLine(
                     "Escolha o histórico a ser exibido:" +
-                    "1 - Histórico de Vendas" +
-                    "2 - Histórico de compras" +
-                    "9 - Voltar ao menu anterior"
+                    "\n1 - Histórico de Vendas" +
+                    "\n2 - Histórico de compras" +
+                    "\n9 - Voltar ao menu anterior"
                 );
-                opt = Int16.Parse(Console.ReadLine());
-            }while (opt != 9);
+                opt4 = Int16.Parse(Console.ReadLine());
+            }while (opt4 != 9);
         }
     }
 }
